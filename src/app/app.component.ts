59d26cca277d108930376c83332fb1bb0b40c63a
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2024-05-03 09:13:43 -0500

    working on componenet practice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ServersComponent } from './servers/servers.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ServersComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
