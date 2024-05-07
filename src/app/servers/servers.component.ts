94cfc599e54f9936a72485e8ab788be9f5c0b295
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2024-05-07 11:21:29 -0500

    close enough practice

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,10 +1,11 @@
+import { NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-servers',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, NgStyle],
   templateUrl: './servers.component.html',
   styleUrl: './servers.component.css'
 })
@@ -37,6 +38,12 @@ this.buttonClicks.push(this.count)
 }
 
 
+getColor(){
+ return this.count>5 ? 'white':'black'
+
+
+
+}
 
 
 
