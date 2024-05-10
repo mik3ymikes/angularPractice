5c4dcd47944179befbab9ec9b5a6b9cf4cb46c3f
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2024-05-10 12:24:47 -0500

    game

---
+++ src/app/even/even.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-even',
+  standalone: true,
+  imports: [],
+  templateUrl: './even.component.html',
+  styleUrl: './even.component.css'
+})
+export class EvenComponent {
+
+}
---
+++ src/app/game-control/game-control.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-control',
+  standalone: true,
+  imports: [],
+  templateUrl: './game-control.component.html',
+  styleUrl: './game-control.component.css'
+})
+export class GameControlComponent {
+
+}
