56ffe4b15304dd8238b714a29c241ae73f05291f
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2024-05-22 12:47:29 -0500

    setting up users

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { TestyComponent } from './testy/testy.component';
 import { Servers3Component } from './servers3/servers3.component';
+import { UsersComponent } from './users/users.component';
 
 export const routes: Routes = [
-  {path: '', component:Servers3Component}
+  {path: 'server', component:Servers3Component},
+  {path: 'users', component:Servers3Component}
 ];
