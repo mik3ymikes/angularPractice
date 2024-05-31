41d397f0379f3c2813b6690c5f36d4f24abf1a93
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2024-05-31 15:59:18 -0500

    more on obser

--- src/app/logging.service.ts
+++ src/app/logging.service.ts
@@ -4,9 +4,9 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class LoggingService {
-  logStatusChage(){
+  logStatusChage() {
     console.log('a server changedf')
   }
-//studying Observables
+  //studying Observables round 2
   constructor() { }
 }
